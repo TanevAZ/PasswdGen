79ac9e6d2632c347b967d5f8a7486433fadd6a38
Tanev <66444552+TanevAZ@users.noreply.github.com>
2022-10-26 15:00:51 +0200

    password generator

--- main.py
+++ main.py
@@ -1,30 +1,48 @@
 import random
 import string
+import os
 
-def password_generator():
-    length = int(input("How many characters : "))
-    nbOfPaswd = int(input("How many passwords : "))
-    spchars = input("Would you like special characters (y/n) : ")
-    numbers = input("Would you like numbers (y/n) : ")
-    uppercase = input("Would you like uppercase letters (y/n) : ")
-    lowercase = input("Would you like lowercase letters (y/n) : ")
+def PasswdGen():
+    password_length = int(input("Enter the length of password : "))
+    password_count = int(input("How many passwords do you want to generate : "))
+    save_to_file = input("Do you want to save the passwords to a file (y/n) : ")
+    passwdChars = string.ascii_letters + string.digits + string.punctuation + random.choice(string.ascii_uppercase) + random.choice(string.ascii_lowercase) + random.choice(string.digits) + random.choice(string.punctuation)
+    password = []
+    passwdList = []
 
+    for x in range(0, password_count):
+        for y in range(0, password_length):
+            password.append(random.choice(passwdChars))
+        passwdList.append(''.join(password))
+        password = []
 
-    for i in range(nbOfPaswd):
-        password = ""
-        for i in range(length):
-            if spchars == "y":
-                password += random.choice(string.punctuation)
-            if numbers == "y":
-                password += random.choice(string.digits)
-            if uppercase == "y":
-                password += random.choice(string.ascii_uppercase)
-            if lowercase == "y":
-                password += random.choice(string.ascii_lowercase)
-
+    for password in passwdList:
         print(password)
 
-    with open("passwords.txt", "a") as file:
-        file.write(password + " ")
+    if save_to_file == 'y':
+        filename = input("Enter the file name 
+        desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+        filePath = input(f"Enter the file path (default path : {desktop}\{filename} ) : ")
+                         
+        if filePath == '':
+            filePath = os.path.join(os.path.expanduser('~'), 'Desktop', filename)
+                         
+            with open(filePath, 'w') as file:
+                for password in passwdList:
+                    file.write(password + ' ' + '\n')
+        else:
+            file = open(os.path.join(filePath, filename), "w")
 
-password_generator()
+            for password in passwdList:
+                file.write(password + " " + '\n')
+                         
+        file.close()
+
+        print("Passwords saved to file : " + filePath)
+    else:
+        print("Passwords not saved to file.")
+
+    input("Press enter to exit.")
+    exit()
+
+PasswdGen()
